f1fa8daa4731ffb62a7d9780dcc46afebe031a51
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-08-25 14:27:47 -0400

    Adding Led class

--- src/main.cpp
+++ src/main.cpp
@@ -59,50 +59,90 @@ public:
   }
 };
 
+class Led
+{
+private:
+  unsigned int pin;
+  unsigned char state;
+
+public:
+  Led() : pin(0), state(LOW)
+  {
+  }
+
+  Led(unsigned int pin)
+  {
+    this->pin = pin;
+    pinMode(pin, OUTPUT);
+    state = LOW;
+  }
+
+  void turnOn()
+  {
+    digitalWrite(pin, HIGH);
+    state = HIGH;
+  }
+
+  void turnOff()
+  {
+    digitalWrite(pin, LOW);
+    state = LOW;
+  }
+};
+
 #define LED_QUANTITY 8
 
 class LedController
 {
 private:
+  Led usedLeds[LED_QUANTITY];
+  unsigned int lastLed = 0;
   const unsigned int stride = 6;
-  const unsigned int usedPins[LED_QUANTITY] = {2, 3, 4, 5, 6, 7, 8, 9};
 
 public:
   LedController()
   {
-    for (int led = 0; led < LED_QUANTITY; led++)
-      pinMode(usedPins[led], OUTPUT);
   }
 
-  void updateLeds(unsigned long distance)
+  void addLed(unsigned int pin)
   {
-    for (int led = 0; led < LED_QUANTITY; led++)
+    usedLeds[lastLed] = Led(pin);
+    lastLed++;
+  }
+
+  void updateLeds(unsigned long actualDistance)
+  {
+    for (int led = 0; led <= lastLed; led++)
     {
       unsigned long fixedDistance = (led + 1) * stride;
-      controlLed(usedPins[led], distance, fixedDistance);
+      controlLed(usedLeds[led], actualDistance, fixedDistance);
     }
   }
 
-  void controlLed(unsigned int pin, unsigned long actualDistance, unsigned long fixedDistance)
+  void controlLed(Led &led, unsigned long actualDistance, unsigned long fixedDistance)
   {
     if (isInRange(actualDistance, fixedDistance))
-      digitalWrite(pin, HIGH);
+      led.turnOn();
     else
-      digitalWrite(pin, LOW);
+      led.turnOff();
   }
 
   bool isInRange(unsigned long actualDistance, unsigned long fixedDistance)
   {
-    return actualDistance <= fixedDistance;
+    return actualDistance < fixedDistance;
   }
 };
 
-UltrasonicSensor ultrasonicSensor(10, 11); // Trigger on pin 10, echo on pin 11
+UltrasonicSensor ultrasonicSensor(2, 5); // Trigger on pin 10, echo on pin 11
 LedController ledController;
 
 void setup()
 {
   Serial.begin(9600);
+  ledController.addLed(13);
+  ledController.addLed(14);
+  ledController.addLed(26);
+  ledController.addLed(33);
 }
 
 void loop()
